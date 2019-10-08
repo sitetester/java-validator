21391d592100d89392b6c85958aa2b839d54a502
sitetester <sitetester2010@gmail.com>
2019-10-08 18:54:34 +0300

    first commit

---
+++ src/main/java/com/example/app/Validator.java
@@ -0,0 +1,231 @@
+package com.example.app;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validator {
+
+    public boolean isBoolean(String str) {
+        return Boolean.parseBoolean(str);
+    }
+
+    public boolean isBooleanTrue(String str) {
+        boolean b = Boolean.parseBoolean(str);
+        return b == true;
+    }
+
+    public boolean isBooleanFalse(String str) {
+        boolean b = Boolean.parseBoolean(str);
+        return b == false;
+    }
+
+    public boolean isInt(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public boolean isEmpty(String str) {
+        return str.trim().length() == 0;
+    }
+
+    public boolean isLowercase(String str) {
+        return str.toLowerCase() == str;
+    }
+
+    public boolean isUppercase(String str) {
+        return str.toUpperCase() == str;
+    }
+
+    public boolean hasSpecialChar(String str) {
+        Pattern p = Pattern.compile("[^a-zA-Z0-9]");
+        Matcher m = p.matcher(str);
+        return  m.find();
+    }
+
+    public boolean isEmail(String str) {
+        Pattern p = Pattern.compile("^[a-zA-Z0-9._]+@[a-zA-Z]+\\.[a-zA-Z]{3}$");
+        Matcher m = p.matcher(str);
+        return m.find();
+    }
+
+    /*
+    public boolean isInIntRange() {
+
+    }
+
+    public boolean isUnicode(String str) {
+
+    }
+    public boolean isInCharRange() {
+
+    }
+
+    public boolean isAlpha() {
+
+    }
+
+    public boolean isAlphaNum() {
+
+    }
+
+    public boolean isCtrl() {
+
+    }
+
+    public boolean isDigit() {
+
+    }
+
+    // ctype_graph ?
+    public boolean isGraph() {
+
+    }
+
+    public boolean isPrint() {
+
+    }
+
+    public boolean isPunct() {
+
+    }
+
+    public boolean isSpace() {
+
+    }
+
+    // ctype_xdigit ?
+    public boolean isXDigit() {
+
+    }
+
+    public boolean hasLength() {
+
+    }
+
+    public boolean isUrl() {
+
+    }
+
+    public boolean regexMatch() {
+
+    }
+
+    public boolean isIp() {
+
+    }
+
+    public boolean isJson() {
+
+    }
+
+    public boolean isUuid() {
+
+    }
+
+    public boolean equalTo() {
+
+    }
+
+    public boolean strictlyEqualTo() {
+
+    }
+
+    public boolean isPositiveInt() {
+
+    }
+
+    public boolean isNegativeInt() {
+
+    }
+    public boolean isBase32() {
+
+    }
+    public boolean isBase64() {
+
+    }
+
+
+  public boolean isAscii() {
+
+    }
+
+    public boolean lessThan() {
+
+    }
+
+    public boolean greaterThan() {
+
+    }
+
+    public boolean divisibleBy() {
+
+    }
+
+    public boolean isPositiveInt() {
+
+    }
+
+    public boolean isDate() {
+
+    }
+
+    public boolean isTime() {
+
+    }
+
+    public boolean isDateTime() {
+
+    }
+
+    public boolean isTimezone() {
+
+    }
+
+    // take from wiki
+    public boolean isLanguageCode() {
+
+    }
+
+    // a string (or object with a __toString() method) path to an existing file;
+    public boolean isFile() {
+
+    }
+
+    public boolean isOneOfValues() {
+
+    }
+
+    public boolean isImage() {
+
+    }
+
+    public boolean isMimeType() {
+
+    }
+
+    public boolean isCurrency() {
+
+    }
+
+    public boolean isIban() {
+
+    }*/
+
+    // isCreditCard
+    // isDecimal
+    // isHexColor
+    // isHexadecimal
+    // isIdentityCard
+    // isISBN
+    // isISO8601
+    // isRFC3339
+    // isJWT
+    // isLatLong
+    // isMACAddress
+    // isMimeType
+    // isMultibyte(str)
+}
---
+++ src/test/java/com/example/app/ValidatorTest.java
@@ -0,0 +1,184 @@
+package com.example.app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ValidatorTest {
+
+    @Test
+    public void isBoolean() {
+        assertTrue(
+                new Validator().isBoolean("true")
+        );
+
+        assertFalse(
+                new Validator().isBoolean("false")
+        );
+
+        assertFalse(
+                new Validator().isBoolean("123")
+        );
+
+        assertFalse(
+                new Validator().isBoolean("0")
+        );
+    }
+
+    @Test
+    public void isBooleanTrue() {
+        assertTrue(
+                new Validator().isBooleanTrue("true")
+        );
+
+        assertFalse(
+                new Validator().isBooleanTrue("1")
+        );
+
+        assertFalse(
+                new Validator().isBooleanTrue("0")
+        );
+
+        assertTrue(
+                new Validator().isBooleanTrue(String.valueOf(true))
+        );
+    }
+
+    @Test
+    public void isBooleanFalse() {
+        assertTrue(
+                new Validator().isBooleanFalse("false")
+        );
+
+        assertTrue(
+                new Validator().isBooleanFalse("0")
+        );
+
+        assertTrue(
+                new Validator().isBooleanFalse(String.valueOf(false))
+        );
+    }
+
+    @Test
+    public void isInt() {
+        assertTrue(
+                new Validator().isInt("123")
+        );
+
+        assertFalse(
+                new Validator().isInt("a")
+        );
+
+        assertFalse(
+                new Validator().isInt("#@1")
+        );
+    }
+
+    @Test
+    public void isEmpty() {
+        assertTrue(
+                new Validator().isEmpty("")
+        );
+
+        assertTrue(
+                new Validator().isEmpty(" ")
+        );
+
+        assertTrue(
+                new Validator().isEmpty("  ")
+        );
+
+        assertTrue(
+                new Validator().isEmpty("\t")
+        );
+
+        assertTrue(
+                new Validator().isEmpty("\t\t\t")
+        );
+    }
+
+    @Test
+    public void isLowercase() {
+        assertTrue(
+                new Validator().isLowercase("a")
+        );
+
+        assertFalse(
+                new Validator().isLowercase("A")
+        );
+    }
+
+    @Test
+    public void isUppercase() {
+        assertTrue(
+                new Validator().isUppercase("A")
+        );
+
+        assertFalse(
+                new Validator().isUppercase("a")
+        );
+    }
+
+    @Test
+    public void hasSpecialChar() {
+        assertTrue(
+                new Validator().hasSpecialChar("123%#@ABC)")
+        );
+
+        assertFalse(
+                new Validator().hasSpecialChar("123")
+        );
+
+        assertFalse(
+                new Validator().hasSpecialChar("abc")
+        );
+    }
+
+    @Test
+    public void isEmail() {
+        assertTrue(
+                new Validator().isEmail("test@yahoo.com")
+        );
+
+        assertTrue(
+                new Validator().isEmail("first.last@yahoo.com")
+        );
+
+        assertTrue(
+                new Validator().isEmail("first_last@yahoo.com")
+        );
+
+        assertTrue(
+                new Validator().isEmail("abc1980@yahoo.com")
+        );
+
+        // starts with number
+        assertFalse(
+                new Validator().isEmail("123test@yahoocom")
+        );
+
+        // missing `@`
+        assertFalse(
+                new Validator().isEmail("testyahoo.com")
+        );
+
+        // missing `.`
+        assertFalse(
+                new Validator().isEmail("test@yahoocom")
+        );
+
+        //  ends with `.comorg`
+        assertFalse(
+                new Validator().isEmail("test@yahoo.comorg")
+        );
+
+        //  ends with `.com.org`
+        assertFalse(
+                new Validator().isEmail("test@yahoo.com.org")
+        );
+
+        assertFalse(
+                new Validator().isEmail("first=last@yahoo.com")
+        );
+    }
+}
